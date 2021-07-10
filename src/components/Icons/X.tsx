81499ad6321b9d247e8e0919e7e1d922d0ea1fd4
SunLu <1011771396@qq.com>
2021-07-10 10:46:41 +0800

    添加代码格式化

--- src/components/Icons/X.tsx
+++ src/components/Icons/X.tsx
@@ -1,22 +1,18 @@
-
-import React from "react"
-import Svg, {
-  Path,
-  Circle
-} from 'react-native-svg';
-import { Animated } from 'react-native';
-import { useAnimated } from "./hooks"
-import { IconsProps } from "./"
-const AnimatedPath = Animated.createAnimatedComponent(Path);
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+import React from 'react'
+import Svg, { Path, Circle } from 'react-native-svg'
+import { Animated } from 'react-native'
+import { useAnimated } from './hooks'
+import { IconsProps } from './'
+const AnimatedPath = Animated.createAnimatedComponent(Path)
+const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 /**
- * @description: 多选
+ * @description: X
  */
 export default (props: IconsProps) => {
-  const { color = "#1890ff", size = 20, visible = true } = props
+  const { color = '#1890ff', size = 20, visible = true } = props
   const { fadeAnim } = useAnimated({ visible })
   return (
-    <Svg height={size} width={size} viewBox={`0 0 20 20`} fill="none" >
+    <Svg height={size} width={size} viewBox={`0 0 20 20`} fill="none">
       <AnimatedCircle
         cx={10}
         cy={10}
@@ -26,8 +22,20 @@ export default (props: IconsProps) => {
         fill={color}
         opacity={fadeAnim}
       />
-      <AnimatedPath d={`M 6 6 l 8 8`} stroke="#fff" strokeWidth={2} fill="none" opacity={fadeAnim} />
-      <AnimatedPath d={`M 6 14 l 8 -8`} stroke="#fff" strokeWidth={2} fill="none" opacity={fadeAnim} />
+      <AnimatedPath
+        d={`M 6 6 l 8 8`}
+        stroke="#fff"
+        strokeWidth={2}
+        fill="none"
+        opacity={fadeAnim}
+      />
+      <AnimatedPath
+        d={`M 6 14 l 8 -8`}
+        stroke="#fff"
+        strokeWidth={2}
+        fill="none"
+        opacity={fadeAnim}
+      />
     </Svg>
   )
 }
