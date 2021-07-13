bb61e5e599d09238f0c7258411298fd0c9199bf8
SunLu <1011771396@qq.com>
2021-07-13 08:26:53 +0800

    feat:消息

--- src/App.tsx
+++ src/App.tsx
@@ -14,39 +14,45 @@ import Animated from './components/Animated'
 import RadioDemo from './components/Radio/demo'
 import CheckBox from './components/CheckBox/demo'
 import Select from './components/Select/demo'
+import JPush from './JPush'
 
 // import Demo from "./Demo"
 // import Demo from "./components/Table/demo"
 const App = () => {
   // return <Demo />
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle={'dark-content'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View style={{ height: 100 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>动画图标</Text>
-          <Animated />
-        </View>
+      <JPush>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View style={{ height: 100 }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>动画图标</Text>
+            <Animated />
+          </View>
 
-        <View style={{ height: 100 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-            单选框
-          </Text>
-          <RadioDemo />
-        </View>
-        <View style={{ height: 100 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-            多选框
-          </Text>
-          <CheckBox />
-        </View>
-        <View style={{ height: 100 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-            下拉选择框
-          </Text>
-          <Select />
-        </View>
-      </ScrollView>
+          <View style={{ height: 100 }}>
+            <Text
+              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+              单选框
+            </Text>
+            <RadioDemo />
+          </View>
+          <View style={{ height: 100 }}>
+            <Text
+              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+              多选框
+            </Text>
+            <CheckBox />
+          </View>
+          <View style={{ height: 100 }}>
+            <Text
+              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+              下拉选择框
+            </Text>
+            <Select />
+          </View>
+        </ScrollView>
+      </JPush>
     </SafeAreaView>
   )
 }
---
+++ src/JPush/index.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableHighlight, Alert } from 'react-native'
+import JPush from 'jpush-react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  setBtnStyle: {
+    width: 320,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#3e83d7',
+    borderRadius: 8,
+    backgroundColor: '#3e83d7',
+    padding: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: '#ffffff',
+  },
+})
+
+class Button extends React.Component {
+  render() {
+    return (
+      <TouchableHighlight
+        onPress={this.props.onPress}
+        underlayColor="#e4083f"
+        activeOpacity={0.5}>
+        <View style={styles.setBtnStyle}>
+          <Text style={styles.textStyle}>{this.props.title}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    JPush.init()
+    //连接状态
+    this.connectListener = result => {
+      // eslint-disable-next-line no-console
+      console.log('connectListener:' + JSON.stringify(result))
+    }
+    JPush.addConnectEventListener(this.connectListener)
+    //通知回调
+    this.notificationListener = result => {
+      // eslint-disable-next-line no-console
+      console.log('notificationListener:' + JSON.stringify(result))
+    }
+    JPush.addNotificationListener(this.notificationListener)
+    //本地通知回调
+    this.localNotificationListener = result => {
+      // eslint-disable-next-line no-console
+      console.log('localNotificationListener:' + JSON.stringify(result))
+    }
+    JPush.addLocalNotificationListener(this.localNotificationListener)
+    // //自定义消息回调
+    // this.customMessageListener = result => {
+    //   console.log("customMessageListener:" + JSON.stringify(result))
+    // };
+    // JPush.addCustomMessagegListener(this.customMessageListener);
+    //tag alias事件回调
+    this.tagAliasListener = result => {
+      // eslint-disable-next-line no-console
+      console.log('tagAliasListener:' + JSON.stringify(result))
+    }
+    JPush.addTagAliasListener(this.tagAliasListener)
+    //手机号码事件回调
+    this.mobileNumberListener = result => {
+      // eslint-disable-next-line no-console
+      console.log('mobileNumberListener:' + JSON.stringify(result))
+    }
+    JPush.addMobileNumberListener(this.mobileNumberListener)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.props.children}
+        {/* <Button title="setLoggerEnable"
+          onPress={() => {
+            JPush.setLoggerEnable(true)
+          }} /> */}
+        <Button
+          title="getRegisterID"
+          onPress={() => {
+            //连接状态
+            // JPush.addConnectEventListener(result => {
+            //   console.log("connectListener:" + JSON.stringify(result))
+            // });
+            JPush.getRegistrationID(result => {
+              Alert.alert('registerID', JSON.stringify(result))
+              // eslint-disable-next-line no-console
+              console.log('registerID:' + JSON.stringify(result))
+            })
+          }}
+        />
+
+        {/*<Button title="addTags"
+                        onPress={() => JPush.addTags({sequence: 1, tags: ["1", "2", "3"]})}/>
+                <Button title="updateTags"
+                        onPress={() => JPush.updateTags({sequence: 2, tags: ["4", "5", "6"]})}/>
+                <Button title="deleteTag"
+                        onPress={() => JPush.deleteTag({sequence: 3, tags: ["4", "5", "6"]})}/>
+                <Button title="deleteTags"
+                        onPress={() => JPush.deleteTags({sequence: 4})}/>
+                <Button title="queryTag"
+                        onPress={() => JPush.queryTag({sequence: 4, tag: "1"})}/>
+                <Button title="queryTags"
+                        onPress={() => JPush.queryTags({sequence: 5})}/>
+                <Button title="setAlias"
+                        onPress={() => JPush.setAlias({sequence: 6,alias:"xxx"})}/>
+                <Button title="deleteAlias"
+                        onPress={() => JPush.deleteAlias({sequence: 7})}/>
+                <Button title="queryAlias"
+                        onPress={() => JPush.queryAlias({sequence: 8})}/>
+                <Button title="setMobileNumber"
+                        onPress={() => JPush.setMobileNumber({mobileNumber: "13888888888"})}/>
+                //仅ios
+                <Button title="setBadge"
+                        onPress={() => JPush.setBadge({"badge":1,"appBadge":1})}/>
+                <Button title="initCrashHandler"
+                        onPress={() => JPush.initCrashHandler()}/>
+                <Button title="addLocalNotification"
+                        onPress={() => JPush.addLocalNotification({
+                            messageID: "123456789",
+                            title: "title123",
+                            content: "content123",
+                            extras: {"key123": "value123"}
+                        })}/>
+                <Button title="removeLocalNotification"
+                        onPress={() => JPush.removeLocalNotification({messageID: '123456789'})}/>*/}
+      </View>
+    )
+  }
+}
