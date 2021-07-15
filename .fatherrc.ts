7e701aedff47c242835baa47b97763fd5293e560
SunLu <1011771396@qq.com>
2021-07-15 08:07:02 +0800

    fix:添加打包工具

---
+++ .fatherrc.ts
@@ -0,0 +1,5 @@
+export default {
+  entry: './src/index.ts',
+  esm: { type: 'babel', minify: true },
+  cjs: { type: 'babel', minify: true },
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { default as CheckBox } from './components/CheckBox'
+export {
+  CheckBoxIcon,
+  RadioIcon,
+  CheckMarkIcon,
+  XIcon,
+  UpIcon,
+  DownIcon,
+  useAnimated,
+} from './components/Icons'
+export { default as Radio } from './components/Radio'
+export { default as Select } from './components/Select'
+export { default as Table } from './components/Table'
