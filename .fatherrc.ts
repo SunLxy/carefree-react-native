2cfd1bdff255d8953b5d40f7b7991cf86d1e0366
SunLu <1011771396@qq.com>
2021-07-15 09:13:46 +0800

    feat:打包工具

--- .fatherrc.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  entry: './src/index.ts',
-  esm: { type: 'babel', minify: true },
-  cjs: { type: 'babel', minify: true },
-}
--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Animated from './components/Animated'
 import RadioDemo from './components/Radio/demo'
 import CheckBox from './components/CheckBox/demo'
 import Select from './components/Select/demo'
-import JPush from './JPush'
 
 // import Demo from "./Demo"
 // import Demo from "./components/Table/demo"
@@ -23,36 +22,31 @@ const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle={'dark-content'} />
-      <JPush>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View style={{ height: 100 }}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>动画图标</Text>
-            <Animated />
-          </View>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={{ height: 100 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>动画图标</Text>
+          <Animated />
+        </View>
 
-          <View style={{ height: 100 }}>
-            <Text
-              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-              单选框
-            </Text>
-            <RadioDemo />
-          </View>
-          <View style={{ height: 100 }}>
-            <Text
-              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-              多选框
-            </Text>
-            <CheckBox />
-          </View>
-          <View style={{ height: 100 }}>
-            <Text
-              style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-              下拉选择框
-            </Text>
-            <Select />
-          </View>
-        </ScrollView>
-      </JPush>
+        <View style={{ height: 100 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+            单选框
+          </Text>
+          <RadioDemo />
+        </View>
+        <View style={{ height: 100 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+            多选框
+          </Text>
+          <CheckBox />
+        </View>
+        <View style={{ height: 100 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
+            下拉选择框
+          </Text>
+          <Select />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
