c1b157beea5a26e5f0163ade888552e69ef973b1
SunLxy <1011771396@qq.com>
2021-10-31 09:17:39 +0800

    released v0.0.37

--- src/index.ts
+++ src/index.ts
@@ -13,31 +13,51 @@ export {
   StarIcon,
 } from './components/Icons'
 export * from './components/Icons'
+
 export { default as CheckBox } from './components/CheckBox'
 export * from './components/CheckBox'
-export { default as Input } from './components/Input'
-export * from './components/Input'
+
 export { default as Radio } from './components/Radio'
 export * from './components/Radio'
+
+export { default as CheckRadio } from './components/CheckRadio'
+export * from './components/CheckRadio'
+
+export { default as Input } from './components/Input'
+export * from './components/Input'
+
 export { default as Select } from './components/Select'
 export * from './components/Select'
+
 export { default as Table } from './components/Table'
 export * from './components/Table'
+
 export { default as ModalLay } from './components/ModalLay'
 export * from './components/ModalLay'
+
 export { default as Card } from './components/Card'
 export * from './components/Card'
+
 export { default as Form } from './components/Form'
 export * from './components/Form'
+
 export { default as FuzzySearch } from './components/FuzzySearch'
 export * from './components/FuzzySearch'
+
+export { default as Search } from './components/Search'
+export * from './components/Search'
+
 export { default as Tree } from './components/Tree'
 export * from './components/Tree'
+
 export { default as SimpleForm } from './components/SimpleForm'
 export * from './components/SimpleForm'
+
 export { default as Button } from './components/Button'
 export * from './components/Button'
+
 export { default as TabNav } from './components/TabNav'
 export * from './components/TabNav'
+
 export { default as TabBar } from './components/TabBar'
 export * from './components/TabBar'
