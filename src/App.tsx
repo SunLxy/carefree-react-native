a942f110175ef33cfff4568c48220882569d8e8b
SunLxy <1011771396@qq.com>
2021-12-29 06:38:44 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ import Search from './components/Search/demo'
 import Swiper from './components/Swiper/demo'
 import PaneDate from './components/PaneDate/demo'
 import Pagination from './components/Pagination/demo'
-import ReactNativeBlobUtil from 'react-native-blob-util'
 
 // global.Person = "222"
 // global.request = (url: RequestInfo, options?: RequestInit) => {
@@ -52,27 +51,6 @@ const App = () => {
   // eslint-disable-next-line no-console
   console.log(global.request('abdc', {}))
 
-  React.useEffect(() => {
-    ReactNativeBlobUtil.config({
-      // add this option that makes response data to be stored as a file,
-      // this is much more performant.
-      fileCache: true,
-    })
-      .fetch(
-        'GET',
-        'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp09%2F21042G4331941H-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1643198212&t=b322cf4ddedbc61b27732e5a5da74989',
-        {
-          //some headers ..
-        },
-      )
-      .then(res => {
-        // the temp file path
-        // eslint-disable-next-line no-console
-        console.log('The file saved to ', res.path())
-      })
-  }, [])
-
-  // return <Demo />
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle={'dark-content'} />
